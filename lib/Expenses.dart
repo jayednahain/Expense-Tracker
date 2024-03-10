26217e3766fffdd749148e8221b4a71e4e8250c1
jayednahian <jayednahian@yahoo.com>
2024-03-10 23:55:15 +0600

    99: added dummy data for expense list show

--- lib/Expenses.dart
+++ lib/Expenses.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'Models/Enums.dart';
+import 'Models/ExpenseModal.dart';
+
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -9,6 +12,23 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+
+  final List<ExpenseModal> _RegisteredExpenseModel =[
+    ExpenseModal(
+        title: "title",
+        amount: 5.5,
+        date: DateTime.now(),
+        category: ExpenseCategory.food,
+        paymentMethod: PaymentMethod.cashMoney),
+    ExpenseModal(
+        title: "title",
+        amount: 5.5,
+        date: DateTime.now(),
+        category: ExpenseCategory.food,
+        paymentMethod: PaymentMethod.cashMoney)
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
