21d1f10956d9622d32258bcac6d77f82e9bf1cde
jayednahian <jayednahian@yahoo.com>
2024-03-24 01:47:52 +0600

    124:App bar color added

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,23 @@ void main() {
   runApp(const MyApp());
 }
 
+
+var KcolorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: KcolorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: KcolorScheme.onPrimaryContainer,
+          foregroundColor: KcolorScheme.primaryContainer,
+        )
+      ),
       debugShowCheckedModeBanner: false,
       home: Expenses()
     );
